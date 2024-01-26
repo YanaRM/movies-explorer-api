7327fa141cea1176fa7070669130bfe16841b469
YanaRM <yana.r.max@yandex.ru>
2024-01-27 00:08:08 +0500

    ...

--- controllers/users.js
+++ controllers/users.js
@@ -78,7 +78,8 @@ module.exports.login = (req, res, next) => {
             .cookie('jwt', token, {
               maxAge: 3600000,
               httpOnly: true,
-              sameSite: 'none',
+              Secure,
+              'SameSite=none'
             })
             .send({
               _id: user._id,
